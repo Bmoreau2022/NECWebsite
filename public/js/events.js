fa425a99f91e99f533bb3b0a82f6738998baaf9c
NMyers-dev <58642147+NMyers-dev@users.noreply.github.com>
2022-05-01 12:13:51 -0400

    pushed

---
+++ public/js/events.js
@@ -0,0 +1,66 @@
+window.addEventListener('resize', function(){
+    addRequiredClass();
+})
+
+
+function addRequiredClass() {
+    if (window.innerWidth < 860) {
+        document.body.classList.add('mobile')
+    } else {
+        document.body.classList.remove('mobile')
+    }
+}
+
+window.onload = addRequiredClass
+
+let hamburger = document.querySelector('.hamburger')
+let mobileNav = document.querySelector('.nav-list')
+
+let bars = document.querySelectorAll('.hamburger span')
+
+let isActive = false
+
+hamburger.addEventListener('click', function() {
+    mobileNav.classList.toggle('open')
+    if(!isActive) {
+        bars[0].style.transform = 'rotate(45deg)'
+        bars[1].style.opacity = '0'
+        bars[2].style.transform = 'rotate(-45deg)'
+        isActive = true
+    } else {
+        bars[0].style.transform = 'rotate(0deg)'
+        bars[1].style.opacity = '1'
+        bars[2].style.transform = 'rotate(0deg)'
+        isActive = false
+    }
+})
+
+$('.carousel[data-type="multi"] .item').each(function(){
+    var next = $(this).next(); // grabs the next sibling of the carouselGrid
+    if (!next.length) { // if ther isn't a next
+        next = $(this).siblings(':first'); // this is the first
+    }
+    next.children(':first-child').clone().appendTo($(this)); // put the next ones on the array
+
+
+
+    for (var i=0;i<3;i++) { // THIS LOOP SPITS OUT EXTRA ITEMS TO THE CAROUSEL
+        next=next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
+
+$(document).ready(() => {
+    $.getJSON('/get_current_user').done((data) => {
+        if (data.message === "success") {
+            const user = data.data;
+            $('.login').remove();
+            $('#showname').text(user.fullname);
+        }else{
+            $('.logout').remove()
+        }
+    })
+})
