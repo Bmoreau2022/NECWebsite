4c231e126dee22f4e665b0e4c8e60006c4336235
Bmoreau2022 <57045639+Bmoreau2022@users.noreply.github.com>
2022-05-02 22:22:22 -0400

    veg

--- public/js/nec_resources.js
+++ public/js/nec_resources.js
@@ -34,3 +34,15 @@ hamburger.addEventListener('click', function() {
         isActive = false
     }
 })
+
+$(document).ready(() => {
+    $.getJSON('/get_current_user').done((data) => {
+        if (data.message === "success") {
+            const user = data.data;
+            //$('.login').remove();
+            $('#showname').text(user.fullname);
+        }else{
+            $('.logout').remove()
+        }
+    })
+})
